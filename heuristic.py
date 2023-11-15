dd6014d33e5918179a747998e3b56e313c0e2ac5
Ari Glockner <ari@glockners.net>
2023-11-15 14:58:25 -0500

    Fixed Node class and made heuristic.py match format from node.py
    Signed-off-by: Ari Glockner <ari@glockners.net>

--- heuristic.py
+++ heuristic.py
@@ -6,7 +6,7 @@ import random
 from node import Node
 
 
-def manhattan_distance(node1: Node, node2: Node) -> float:
+def manhattan_distance(node1: tuple[int, int], node2: tuple[int, int]) -> int:
     """
     Calculates the Manhattan distance between two nodes.
 
@@ -14,10 +14,10 @@ def manhattan_distance(node1: Node, node2: Node) -> float:
     :param node2: node 2
     :return: the Manhattan distance between the two nodes
     """
-    return abs(node1.pos[0] - node2.pos[0]) + abs(node1.pos[1] - node2.pos[1])
+    return abs(node1[0] - node2[0]) + abs(node1[1] - node2[1])
 
 
-def bozo_distance() -> float:
+def bozo_distance() -> int:
     """
     Calculates the Bozo distance between two nodes.
 
